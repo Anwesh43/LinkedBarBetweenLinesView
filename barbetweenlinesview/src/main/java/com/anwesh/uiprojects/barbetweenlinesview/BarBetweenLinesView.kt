74839a1e0a143fb0c9965cefb2b5b14f1ef2aa96
Anwesh43 <anweshthecool0@gmail.com>
2020-09-20 04:21:41 +0530

    adding constants and libraries

--- barbetweenlinesview/src/main/java/com/anwesh/uiprojects/barbetweenlinesview/BarBetweenLinesView.kt
+++ barbetweenlinesview/src/main/java/com/anwesh/uiprojects/barbetweenlinesview/BarBetweenLinesView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.barbetweenlinesview
 /**
  * Created by anweshmishra on 20/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#FFEB3B",
+        "#4CAF50",
+        "#2196F3",
+        "#FF5722"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val barSizeFactor : Float = 8.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
