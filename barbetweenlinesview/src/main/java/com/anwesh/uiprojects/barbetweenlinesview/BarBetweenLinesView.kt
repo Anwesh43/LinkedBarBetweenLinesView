59d2b7e72b502f24ef364dc45da0b0459cbe556c
Anwesh43 <anweshthecool0@gmail.com>
2020-09-20 04:51:36 +0530

    adding BarBetweenLinesView class

--- barbetweenlinesview/src/main/java/com/anwesh/uiprojects/barbetweenlinesview/BarBetweenLinesView.kt
+++ barbetweenlinesview/src/main/java/com/anwesh/uiprojects/barbetweenlinesview/BarBetweenLinesView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawBBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBarBetweenLines(scale, w, h, paint)
 }
+
+class BarBetweenLinesView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
