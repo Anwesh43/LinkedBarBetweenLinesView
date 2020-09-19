37f747f56ef3e156d5eb61286d6f178fcc911ad5
Anwesh43 <anweshthecool0@gmail.com>
2020-09-20 04:50:21 +0530

    adding Canvas extension methods

--- barbetweenlinesview/src/main/java/com/anwesh/uiprojects/barbetweenlinesview/BarBetweenLinesView.kt
+++ barbetweenlinesview/src/main/java/com/anwesh/uiprojects/barbetweenlinesview/BarBetweenLinesView.kt
@@ -31,3 +31,43 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBarBetweenLine(scale : Float, w: Float, h : Float, paint : Paint) {
+    val barSize : Float = Math.min(w, h) / barSizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(-w / 2, 0f)
+    drawRect(RectF(0f, -(barSize / 2)  * sf1, barSize, (barSize / 2) * sf1), paint)
+    restore()
+    for (j in 0..1) {
+        save()
+        translate(0f, -barSize / 2 + barSize * j)
+        drawLine(0f, 0f, -(w / 2) * sf2, 0f, paint)
+        restore()
+    }
+    drawLine(0f, 0f, -(w / 2 - barSize) * sf3, 0f, paint)
+}
+
+fun Canvas.drawBarBetweenLines(scale : Float, w : Float, h : Float, paint : Paint) {
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        translate(1f - 2 * j, 1f)
+        drawBarBetweenLine(scale, w, h, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarBetweenLines(scale, w, h, paint)
+}
